15eb52c7d81b6499ebfabe82dedd653a14af6358
Bill Wacek <bill.wacek@gmail.com>
2023-07-26 22:00:19 -0400

    Attempting to fix root/sudo inconsistencies

--- wgetfile.sh
+++ wgetfile.sh
@@ -21,9 +21,9 @@ function log() {
 function rmdocker() {
 if [ $(which docker) ]; then
    dockers=$(docker ps -a --format '{{.Names}}' | sed '/^$/d' | grep -E 'dockserver')
-   $(which docker) stop $dockers > /dev/null
-   $(which docker) rm $dockers > /dev/null
-   $(which docker) system prune -af > /dev/null
+   sudo $(which docker) stop $dockers > /dev/null
+   sudo $(which docker) rm $dockers > /dev/null
+   sudo $(which docker) system prune -af > /dev/null
    unset $dockers
 fi
 }
@@ -58,17 +58,17 @@ sudo $(command -v rm) -rf $remove 1>/dev/null 2>&1 && clear
 unset remove
 
 if [ ! $(which docker) ]; then
-   $(which curl) -fsSL https://get.docker.com -o /tmp/docker.sh && bash /tmp/docker.sh
-   $(which systemctl) reload-or-restart docker.service
+   $(which curl) -fsSL https://get.docker.com -o /tmp/docker.sh && sudo bash /tmp/docker.sh
+   sudo $(which systemctl) reload-or-restart docker.service
 fi
 
 if [ ! $(which docker-compose) ]; then
-    $(which curl) -L --fail https://raw.githubusercontent.com/linuxserver/docker-docker-compose/master/run.sh -o /usr/local/bin/docker-compose
-    $(which ln) -sf /usr/local/bin/docker-compose /usr/bin/docker-compose
-    $(which chmod) +x /usr/local/bin/docker-compose /usr/bin/docker-compose
+   sudo $(which curl) -L --fail https://raw.githubusercontent.com/linuxserver/docker-docker-compose/master/run.sh -o /usr/local/bin/docker-compose
+   sudo $(which ln) -sf /usr/local/bin/docker-compose /usr/bin/docker-compose
+   sudo $(which chmod) +x /usr/local/bin/docker-compose /usr/bin/docker-compose
 fi
 
-[[ ! -d "/opt/dockserver" ]] && mkdir -p /opt/dockserver
+[[ ! -d "/opt/dockserver" ]] && sudo mkdir -p /opt/dockserver
 
 file=/opt/dockserver/.installer/dockserver
 store=/usr/bin/dockserver
@@ -84,20 +84,20 @@ else
 fi
 done
 
-if test -f "/usr/bin/dockserver"; then $(which rm) /usr/bin/dockserver ; fi
+if test -f "/usr/bin/dockserver"; then sudo $(which rm) /usr/bin/dockserver ; fi
 if [[ $EUID != 0 ]]; then
-    $(which chown) -R $(whoami):$(whoami) /opt/dockserver
-    $(which usermod) -aG sudo $(whoami)
-    cp /opt/dockserver/.installer/dockserver /usr/bin/dockserver
-    ln -sf /usr/bin/dockserver /bin/dockserver
-    chmod +x /usr/bin/dockserver
-    $(which chown) $(whoami):$(whoami) /usr/bin/dockserver 
+   sudo $(which chown) -R $(whoami):$(whoami) /opt/dockserver
+   sudo $(which usermod) -aG sudo $(whoami)
+   sudo cp /opt/dockserver/.installer/dockserver /usr/bin/dockserver
+   sudo ln -sf /usr/bin/dockserver /bin/dockserver
+   sudo chmod +x /usr/bin/dockserver
+   sudo $(which chown) $(whoami):$(whoami) /usr/bin/dockserver 
 else 
-    $(which chown) -R 1000:1000 /opt/dockserver
-    cp /opt/dockserver/.installer/dockserver /usr/bin/dockserver
-    ln -sf /usr/bin/dockserver /bin/dockserver
-    chmod +x /usr/bin/dockserver
-    $(which chown) -R 1000:1000 /usr/bin/dockserver
+   sudo $(which chown) -R 1000:1000 /opt/dockserver
+   sudo cp /opt/dockserver/.installer/dockserver /usr/bin/dockserver
+   sudo ln -sf /usr/bin/dockserver /bin/dockserver
+   sudo chmod +x /usr/bin/dockserver
+   sudo $(which chown) -R 1000:1000 /usr/bin/dockserver
 fi
 
 printf "
